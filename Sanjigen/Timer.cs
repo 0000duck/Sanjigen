8088d35102743a2e4f8742c1fa875229a0c4e544
alcexhim <alcexhim@gmail.com>
2014-06-27 16:19:12 -0400

    Added Timer implementation using glutTimerFunc

---
+++ Sanjigen/Timer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caltron
+{
+	public class Timer
+	{
+		public Timer()
+		{
+			_tickDelegate = new Internal.FreeGLUT.Delegates.TimerCallback(_tick);
+		}
+		public Timer(int interval) : this()
+		{
+			mvarInterval = interval;
+		}
+
+		private int mvarInterval = 0;
+		public int Interval { get { return mvarInterval; } set { mvarInterval = value; } }
+
+		private Internal.FreeGLUT.Delegates.TimerCallback _tickDelegate = null;
+		private void _tick(int value)
+		{
+			OnTick(EventArgs.Empty);
+			if (mvarEnabled)
+			{
+				Caltron.Internal.FreeGLUT.Methods.glutTimerFunc(mvarInterval, _tickDelegate, 0);
+			}
+		}
+		
+		public event EventHandler Tick;
+		protected virtual void OnTick(EventArgs e)
+		{
+			if (Tick != null) Tick(this, e);
+		}
+
+		private bool mvarEnabled = false;
+		public bool Enabled
+		{
+			get { return mvarEnabled; }
+			set
+			{
+				bool e = mvarEnabled;
+				mvarEnabled = value;
+				
+				if (e == false && value == true)
+				{
+					Caltron.Internal.FreeGLUT.Methods.glutTimerFunc(mvarInterval, _tickDelegate, 0);
+				}
+			}
+		}
+	}
+}
