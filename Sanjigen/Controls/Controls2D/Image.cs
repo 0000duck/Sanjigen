c2e791e95ab1ee574f44e48d1d42b6d45e8210e4
alcexhim <alcexhim@gmail.com>
2014-07-05 23:36:06 -0400

    Added Image and Screen controls

---
+++ Sanjigen/Controls/Controls2D/Image.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor;
+using UniversalEditor.ObjectModels.Multimedia.Picture;
+
+namespace Caltron.Controls.Controls2D
+{
+	public enum ImageScaleMode
+	{
+		None,
+		Center,
+		Stretch
+	}
+	public class Image : Control2D
+	{
+		private ImageScaleMode mvarImageScaleMode = ImageScaleMode.None;
+		public ImageScaleMode ImageScaleMode { get { return mvarImageScaleMode; } set { mvarImageScaleMode = value; } }
+
+		private PictureObjectModel mvarPicture = null;
+		public PictureObjectModel Picture { get { return mvarPicture; } set { mvarPicture = value; } }
+
+		public Image(PictureObjectModel picture)
+		{
+			mvarPicture = picture;
+		}
+
+		protected internal override void OnRender(RenderEventArgs e)
+		{
+			base.OnRender(e);
+			switch (mvarImageScaleMode)
+			{
+				case Controls2D.ImageScaleMode.Center:
+				{
+					e.Canvas.DrawImage((base.Size.Width - mvarPicture.Width) / 2, (base.Size.Height - mvarPicture.Height) / 2, mvarPicture.Width, mvarPicture.Height, mvarPicture);
+					break;
+				}
+				case Controls2D.ImageScaleMode.None:
+				{
+					e.Canvas.DrawImage(0, 0, mvarPicture.Width, mvarPicture.Height, mvarPicture);
+					break;
+				}
+				case Controls2D.ImageScaleMode.Stretch:
+				{
+					e.Canvas.DrawImage(0, 0, base.Size.Width, base.Size.Height, mvarPicture);
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Sanjigen/Controls/Controls2D/Screen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor;
+
+namespace Caltron.Controls.Controls2D
+{
+	public class Screen : Control2D, IControlContainer
+	{
+		public Screen()
+		{
+			mvarControls = new Control.ControlCollection(this);
+		}
+
+		public Window GetParentWindow()
+		{
+			IControlContainer parent = Parent;
+			while (parent != null)
+			{
+				if (parent is Window) return (parent as Window);
+				parent = parent.Parent;
+			}
+			return null;
+		}
+
+		private Control.ControlCollection mvarControls = null;
+		public Control.ControlCollection Controls { get { return mvarControls; } }
+
+		private Color mvarBackgroundColor = Colors.White;
+		public Color BackgroundColor { get { return mvarBackgroundColor; } set { mvarBackgroundColor = value; } }
+
+		protected internal override void OnCreated(EventArgs e)
+		{
+			base.OnCreated(e);
+			Size = GetParentWindow().Size;
+		}
+
+		protected internal override void OnRender(RenderEventArgs e)
+		{
+			base.OnRender(e);
+
+			e.Canvas.Clear(mvarBackgroundColor);
+			foreach (Control ctl in mvarControls)
+			{
+				BeforeRenderEventArgs bre = new BeforeRenderEventArgs(e.Canvas);
+				ctl.OnBeforeRender(bre);
+
+				RenderEventArgs re = new RenderEventArgs(e.Canvas);
+				ctl.OnRender(re);
+				ctl.OnAfterRender(re);
+			}
+		}
+
+		public void Refresh()
+		{
+			if (Parent != null) Parent.Refresh();
+		}
+	}
+}
